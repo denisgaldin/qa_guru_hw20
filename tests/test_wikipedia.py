85d6aee1924aaa186dce1f68a94a24310601e58b
denisgaldin <denisgaldin@mail.ru>
2025-01-17 21:38:28 +0300

    New

--- tests/test_wikipedia.py
+++ tests/test_wikipedia.py
@@ -1,17 +1,28 @@
-from allure_commons._allure import step
+import allure
 from appium.webdriver.common.appiumby import AppiumBy
 from selene import browser, have
 
 
-def test_search():
-    with step('Skip wellcome screen'):
-        browser.element((AppiumBy.ID, 'org.wikipedia.alpha:id/fragment_onboarding_skip_button')).click()
+def test_wiki():
+    with allure.step("Check open welckome screen"):
+        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/primaryTextView")). \
+            should(have.text("The Free Encyclopedia\n…in over 300 languages"))
 
-    with step('Type search'):
-        browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
-        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type('Appium')
+    with allure.step("Go to second page of welcome screen"):
+        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/fragment_onboarding_forward_button")).click()
+        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/primaryTextView")). \
+            should(have.text("New ways to explore"))
 
-    with step('Verify content found'):
-        results = browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title'))
-        results.should(have.size_greater_than(0))
-        results.first.should(have.text('Appium'))
+    with allure.step("Go to third page of welcome screen"):
+        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/fragment_onboarding_forward_button")).click()
+        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/primaryTextView")). \
+            should(have.text("Reading lists with sync"))
+
+    with allure.step("Go to fourth page of welcome screen"):
+        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/fragment_onboarding_forward_button")).click()
+        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/primaryTextView")). \
+            should(have.text("Data & Privacy"))
+
+    with allure.step("Close welcome screen"):
+        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/fragment_onboarding_done_button")).click()
+        browser.all((AppiumBy.CLASS_NAME, "android.widget.TextView")).first.should(have.text("Search Wikipedia"))
